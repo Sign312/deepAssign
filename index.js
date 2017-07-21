7e37c4f40a6b30f37a26278b7c89a9b9480d551c
胡钺 <shixi_huyue3@staff.sina.com.cn>
2017-07-21 09:50:23 +0800

    修复数组问题

--- index.js
+++ index.js
@@ -6,13 +6,29 @@ const deepAssign = (objTo, ...objs) => {
 };
 
 const deepAssignSingle = (objTo, obj) => {
+  if (obj instanceof Array) {
+    objTo = [];
+    obj.forEach(item => {
+      objTo.push(deepAssignSingle({}, item));
+    });
+    return;
+  }
   let keys = Object.keys(obj);
   keys.forEach(key => {
     if (typeof obj[key] == "object") {
-      if (typeof objTo[key] != "object") {
-        objTo[key] = {};
+      if (obj[key] instanceof Array) {
+        objTo[key] = [];
+        obj[key].forEach(item => {
+          let temp = {};
+          deepAssignSingle(temp, item);
+          objTo[key].push(temp);
+        });
+      } else {
+        if (typeof objTo[key] != "object" || objTo[key] instanceof Array) {
+          objTo[key] = {};
+        }
+        deepAssignSingle(objTo[key], obj[key]);
       }
-      deepAssignSingle(objTo[key], obj[key]);
     } else {
       objTo[key] = obj[key];
     }
